6dedfc5bebebc2139d03df65f1eb9045ff4fc9a1
Yasin Mogultay <yasin.mglty@gmail.com>
2021-02-02 14:34:06 -0600

    Completed JQuery Selectors exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,5 +1,28 @@
 
-
 $(document).ready(function (){
-    alert("Hey you reached JQuery.")
-})
+
+    //1) Id selectors part of exercise
+
+    // var content = $("#header-1").html();
+    // alert(content);
+    //using same id on the elements and alert them, Jquery alerting only the first one
+
+    //2) Class selectors part of exercise
+
+    // $('.codeup').css('border', '1px solid #F00');
+    // when you delete codeup class its no longer bordered and
+    // when you give id codeup another element its not changing cause we selecting class codeup
+
+    //3) Element selectors part of exercise
+
+    $('li').css('font-size', '20px');
+    // $('li').css('background', 'yellow');
+    // $('h1').css('background', 'yellow');
+    // $('p').css('background', 'yellow');
+    var h1Content = $('h1').text();
+    alert(h1Content);
+
+    //4) Multiple selectors part of exercise
+    $('li, p, h1').css('background', 'yellow');
+});
+
